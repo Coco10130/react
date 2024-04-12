a68c6c152651c8b55cd26719eacfb6bcc4e30ed2
John Ric Tenepere <joso.tenepere.up@phinmaed.com>
2024-04-12 15:40:49 +0800

    basta

--- react-api/react-api/src/router.jsx
+++ react-api/react-api/src/router.jsx
@@ -4,6 +4,7 @@ import Register from "./views/Register";
 import GuestLayout from "./components/GuestLayout";
 import DefaultLayout from "./components/DefaultLayout";
 import TodoList from "./views/TodoList";
+import Notes from "./views/Notes";
 
 const router = createBrowserRouter([
     {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
             {
                 path: "/todos",
                 element: <TodoList />,
+            },
+            {
+                path: "/notes",
+                element: <Notes />,
             },
         ],
     },
---
+++ react-api/react-api/src/views/Notes.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/Notes";
+
+function Notes() {
+    return (
+        <>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col">
+                        <h2 className="notes-text">Notes</h2>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Notes;
